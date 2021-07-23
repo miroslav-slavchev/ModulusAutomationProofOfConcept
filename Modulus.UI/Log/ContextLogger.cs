c9116ed5f5bec105e9272dcbff0ad31fb26ecebc
DESKTOP-4R3QEPO\miros <miroslav.svetoslavov.slavchev@gmail.com>
2021-07-23 10:24:43 +0300

    Add log

---
+++ Modulus.UI/Log/ContextLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulus.UI.Log
+{
+    public sealed class ContextLogger
+    {
+        private static ILogger _instance;
+
+        public static ILogger Logger
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var customLogger = ContextLoggerInstance.Logger;
+                    if (customLogger == null)
+                    {
+                        _instance = new DefaultLogger();
+                    }
+                    else
+                    {
+                        _instance = customLogger;
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public static class ContextLoggerInstance
+    {
+        public static ILogger Logger { get; set; }
+    }
+}
---
+++ Modulus.UI/Log/DefaultLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulus.UI.Log
+{
+    public class DefaultLogger : ILogger
+    {
+        public void LogDebug(string message)
+        {
+            string prefix = "-> debug: ";
+            Log(prefix, message);
+        }
+
+        public void LogError(string message)
+        {
+            string prefix = "-> error: ";
+            Log(prefix, message);
+        }
+
+        public void LogInfo(string message)
+        {
+            string prefix = "-> info: ";
+            Log(prefix, message);
+        }
+
+        public void LogWarning(string message)
+        {
+            string prefix = "-> warning: ";
+            Log(prefix, message);
+        }
+
+        private void Log(string prefix, string message)
+        {
+            Console.WriteLine(prefix + message);
+        }
+    }
+}
